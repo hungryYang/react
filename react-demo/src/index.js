b153a5cb51dce062b27fe469a2c00a72a8db886b
hungryYang <ycr1714336458@163.com>
2017-04-12 16:55:51 +0800

    .

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import './index.css';
 import Welcome from './Welcome'
-/*
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root2')
-  );
-}*/
 
-  ReactDOM.render(
-    <Welcome name='yang'/>,
-    document.getElementById('root2')
-  );
-
-// setInterval(tick, 1000);
+ReactDOM.render(
+<Welcome name='yang'/>,
+document.getElementById('root2')
+);
