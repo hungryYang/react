d1cad410a7b0d59a9c6035ee79f1fde4fb6fe942
hungryYang <ycr1714336458@163.com>
2017-04-12 16:28:25 +0800

    展示钩子

--- react-demo/src/Welcome.js
+++ react-demo/src/Welcome.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
-// class Welcome extends React.Component {
-//     render(){
-//         return <h1>Hello,{this.props.name}</h1>
-//     }
-// }
+class Welcome extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            date:new Date()
+        }
+        setInterval(()=>{
+            this.setState({
+                date : new Date()
+            })
+        },1000)
 
-function Welcome(props){
-    return  <h1>Hello,{props.name}</h1>
+        console.log('我已经在 constructor 里将 props 和 state 初始化好了')  
+    }
+    render(){
+        console.log('嗯，这里是 render')
+        return(
+             <div>
+                <h1>Hello,{this.props.name}</h1>
+                <h2>{this.state.date.toString()}</h2>
+            </div>
+        )
+           
+    }
+    componentDidMount(){
+        console.log('已经挂载到页面里了')
+    }
 }
+
 
 export default Welcome
