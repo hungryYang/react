495232fdc651719602d07271040299c9329e6ff3
hungryYang <ycr1714336458@163.com>
2017-04-12 15:35:35 +0800

    组件用法,props的用法

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
+import Welcome from './Welcome'
+/*
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
     document.getElementById('root2')
-);
+  );
+}*/
+
+  ReactDOM.render(
+    <Welcome name='yang'/>,
+    document.getElementById('root2')
+  );
+
+// setInterval(tick, 1000);
