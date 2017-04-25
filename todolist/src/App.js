14369c350905bba33aab712bd86219658deadb1b
hungryYang <ycr1714336458@163.com>
2017-04-25 13:57:04 +0800

    封装TodoInput组件 TodoItem组件

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TodoInput from './TodoInput.js'
+import TodoItem from './TodoItem.js'
 import './App.css';
 
 class App extends Component {
@@ -7,19 +9,24 @@ class App extends Component {
     this.state = {
       newTodo:'test',
       todoList:[
-        {id:1,title:'第一个待办'}
+        {id:1,title:'第一个待办'},
+        {id:2,title:'第二个待办'}
       ]
     }
   }
   render() {
     let todos = this.state.todoList.map((item,index)=>{
-      return <li>item</li>
+      return (
+        <li>
+          <TodoItem todo={item}/>
+        </li>
+      )
     })
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <input type="text" value={this.state.newTodo}/>
+          <TodoInput value={this.state.newTodo}/>
         </div>
         <ol>
           {todos}
