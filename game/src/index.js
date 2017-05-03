d1c9e75400f336a4d1ee0d9594ad7d20d1c8479a
hungryyang <ycr1714336458@163.com>
2017-05-03 20:02:03 +0800

    添加

--- game/src/index.js
+++ game/src/index.js
@@ -3,18 +3,31 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 class Square extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            value : null
+        }
+    }
     render() {
         return (
-            <button className="square">
-
+            <button className="square" onClick={ ()=>{this.setState({value:'X'})} }>
+                {this.state.value}
             </button>
         )
     }
 }
 
 class Board extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            squares:Array(9).fill(null)
+        }
+    }
+
     renderSquare (i){
-        return <Square/>
+        return <Square value={i}/>
     }
 
     render() {
